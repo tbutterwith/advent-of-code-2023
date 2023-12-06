033b4dec0ab776d757bd86b4e32299f60bc2cba7
Tom Butterwith <tbutterwith@gmail.com>
2023-12-06 09:24:46 +0000

    solves day six, part two
    The transition from part one was so simple I forgot to commit that part. A nice easy one today in comparison to the last few days

---
+++ day_six.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	file_lines := ReadFile("inputs/6.txt")
+
+	time := strings.Join(strings.Fields(strings.Split(file_lines[0], ":")[1]), "")
+	distance := strings.Join(strings.Fields(strings.Split(file_lines[1], ":")[1]), "")
+
+	total := 0
+
+	race_time, _ := strconv.Atoi(time)
+	race_distance, _ := strconv.Atoi(distance)
+	for button_time := 0; button_time <= race_time; button_time++ {
+		remaining_time := race_time - button_time
+
+		if (button_time * remaining_time) > race_distance {
+			total += 1
+		}
+	}
+
+	fmt.Println(total)
+}
